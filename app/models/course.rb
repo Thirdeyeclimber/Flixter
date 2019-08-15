1f97bf4bc1cb81b649700f2178cc130cafb78db9
Michael <mr.enviro@gmail.com>
2019-08-15 17:48:52 +0000

    Student Enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	belongs_to :user
 	has_many :sections
+	has_many :enrollments
 
 	validates :title, presence: true
 	validates :description, presence: true
