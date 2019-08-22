77cb0d76f118bd3f8f51333ae6d9dddc8565be80
Michael <mr.enviro@gmail.com>
2019-08-22 15:40:41 +0000

    dashboard feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :sections, only: [:update]
     resources :lessons, only: [:update]
-  	resources :sections, only: [] do 
+  	resources :sections, only: [:update] do 
   		resources :lessons, only: [:new, :create]
   	end
   	resources :courses, only: [:new, :create, :show] do
