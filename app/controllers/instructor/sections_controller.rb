1307997585f1657975d7621a5761154337a4eb77
Michael <mr.enviro@gmail.com>
2019-08-22 17:28:46 +0000

    new section modal

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -3,10 +3,6 @@ class Instructor::SectionsController < ApplicationController
 	before_action :require_authorized_for_current_course, only: [:create]
 	before_action :require_authorized_for_current_section, only: [:update]
 
-	def new
-		@section = Section.new
-	end
-
 	def create
 		@section = current_course.sections.create(section_params)
 		redirect_to instructor_course_path(current_course)
